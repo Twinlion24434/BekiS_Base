5c149aed51ed610887219684e33c18285258241f
Twinlion_TL <150567863+Twinlion24434@users.noreply.github.com>
2025-09-24 19:40:09 +0700

    Update Bekis.py

--- Bekis.py
+++ Bekis.py
@@ -2,11 +2,11 @@ import threading
 import requests
 import sys
 import random
-print '''###############################
+print ('''###############################
 ######  ######  Bekis_HTTP-Attacker #
 ##### () #####   by Twinlion  #
 ####      ####                #
-###############################'''
+###############################''')
 host = raw_input("Url/ip:")
 thread_num = input("threads:")
 
